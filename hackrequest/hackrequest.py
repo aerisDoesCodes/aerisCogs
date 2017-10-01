85d71f474bcd0dea1d74512bd07e464da909f463
aerisDoesCodes <aerisdoescodes@gmail.com>
2017-10-01 16:21:15 +0800

    Fixing shitty error

--- hackrequest/hackrequest.py
+++ hackrequest/hackrequest.py
@@ -119,17 +119,6 @@ class Request:
                         break
                 if game is None:
                     break
-                    platmsg = await self.bot.send_message(author, "What game you want to be hacked?")
-                while True:
-                    plat = await self.bot.wait_for_message(channel=platmsg.channel, author=author, timeout=30)
-                    if plat is None:
-                        await self.bot.send_message(author, "Sorry you took to long, please try again later!")
-                        break
-                    else:
-                        em.add_field(name="Platform", value=game.content, inline=True)
-                        break
-                if plat is None:
-                    break
                 haxmsg = await self.bot.send_message(author, "What are the features you want on the hack?")
                 while True:
                     hax1 = await self.bot.wait_for_message(channel=haxmsg.channel, author=author, timeout=30)
